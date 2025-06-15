ce2a58d16a75079f7a29fcd8449876b9ea3fd04f
dalvin2386 <dalvinsoriano@gmail.com>
2025-06-14 21:15:20 -0600

    Commit fin de la clase

--- CRUDMVVMQ22025/Views/MainView.xaml.cs
+++ CRUDMVVMQ22025/Views/MainView.xaml.cs
@@ -1,9 +1,14 @@
+using CRUDMVVMQ22025.ViewModels;
+
 namespace CRUDMVVMQ22025.Views;
 
 public partial class MainView : ContentPage
 {
+	private MainViewModel viewModel;
 	public MainView()
 	{
 		InitializeComponent();
+		viewModel= new MainViewModel();
+		this.BindingContext= viewModel;
 	}
 }
